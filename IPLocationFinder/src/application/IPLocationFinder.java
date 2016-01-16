1e11e7a83682e3de6ddeb266113e2b80974daecb
Kevin BASCOL <bascol.kevin@hotmail.fr>
2016-01-16 13:08:08 +0100

    Application fonctionnelle

--- IPLocationFinder/src/application/IPLocationFinder.java
+++ IPLocationFinder/src/application/IPLocationFinder.java
@@ -1,12 +1,18 @@
 package application;
 
 import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import net.webservicex.GeoIPServiceSoap;
 
 public class IPLocationFinder {
 
 	public static void main(String[] args) {
-		GeoIP g;
-
+		GeoIPServiceSoap service = new GeoIPService().getGeoIPServiceSoap();
+		GeoIP detIp;
+		
+		for(String ip : args) {
+			detIp = service.getGeoIP(ip);
+			System.out.println(ip + " -> " + detIp.getCountryCode() + " (" + detIp.getCountryName() + ")");
+		}
 	}
-
 }
